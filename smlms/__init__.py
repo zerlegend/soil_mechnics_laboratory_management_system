dae89027d34fd96f89d85f9ceeabf612fe56cde6
zerlegend <zerlegend@gmail.com>
2024-01-15 10:16:11 +0200

    adding __init__.py file

--- smlms/__init__.py
+++ smlms/__init__.py
@@ -4,7 +4,6 @@ from smlms.config import Config
   
 app = Flask(__name__)
 app.secret_key = '78fe2c0421ba6c56cc3bb575b7046451'
-
 app.config.from_object(Config)  # Load configurations
 
 mysql = MySQL(app)
